1f85a5ab26d47267ea384f676cc15142f0f918fe
Joshua Hardman <jhardman0830@gmail.com>
2018-01-22 00:45:00 -0700

    Removing unecessary regex stuffs

--- src/components/Email.js
+++ src/components/Email.js
@@ -1,11 +1,9 @@
 import React, { Component } from "react";
-import PropTypes from "prop-types";
-import classnames from "classnames";
 import Text from "./Text";
 
 class Email extends Component {
   validateEmail = value => {
-    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    const re = /^(([^<>()\]\\.,;:\s@"]+(\.[^<>()\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(value);
   };
 
